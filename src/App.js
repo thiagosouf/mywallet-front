c3314a8b96916369e976e36858dd20c2f7220a6b
thiagosouf <thiagosouf@gmail.com>
2022-05-04 00:54:35 -0300

    arquivos iniciais

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import GlobalStyles from "./globalStyles"; 
+import Login from "./components/Login";
+import Cadastro from "./components/Cadastro";
+import Dashboard from "./components/Dashboard";
+import NovaEntrada from "./components/NovaEntrada";
+import NovaSaida from "./components/NovaSaida";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <GlobalStyles />
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/novaEntrada" element={<NovaEntrada />} />
+                <Route path="/novaSaida" element={<novaSaida />} />
+            </Routes>
+        </BrowserRouter>
+        
+    )}
